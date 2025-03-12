c8b32cd5d250bb4e827a8a22841ac4c037ac4a18
Sitthata <aprils96950@gmail.com>
2025-03-12 13:45:03 +0700

    migrate room to have associations

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -57,6 +57,11 @@ class RoomsController < ApplicationController
     end
   end
 
+  def summary
+    @rooms = Room.all
+    # render action: :summary
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_room
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  resources :messages
+  get "rooms/summary" => "rooms#summary"
   resources :rooms
+  resources :messages
+
+  # Try to solve using nested routes
+  # resources :rooms do
+  #   collection do
+  #     get :summary
+  #   end
+  # end
+
+  # get "/rooms/summary" => "rooms#summary"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
